f77a5a3dcbede55b380365e3339c589600d7b78e
JangHyoJoon-dev <1824252@donga.ac.kr>
2023-05-16 11:18:36 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 import streamlit as st
-x = st.slider('select value')
-st.write(x, 'squared is', x * x)
+
+st.checkbox('yes')
+st.button('click')
+st.radio('pick your gender', ['male', 'female'])
+st.selectbox('pick your gender', ['male', 'female'])
+st.multiselect('choose a planet', ['jupiter', 'mars', 'neptune'])
+st.select_slider('pick a mark', ['bad', 'good', 'execellent'])
+st.slider('pick a number', 0, 50)
